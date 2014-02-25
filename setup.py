7a51f1915816ae64441241975261687ae7eae19e
LittleQ <littleq0903@gmail.com>
2014-02-25 23:50:19 +0800

    distutil

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='Spritz',
+    version='0.1',
+    author='Colin Su',
+    author_email='littleq0903@gmail.com',
+    scripts=['bin/spritz.py'],
+    license='LICENSE',
+    description='Command-line version of Spritz',
+    long_description=open('README.md').read(),
+)
